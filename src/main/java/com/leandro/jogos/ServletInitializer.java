72ddf693202214b12f74699ee3302a862524c115
landocorgas <landocorgas@outlook.com>
2022-07-30 12:52:29 -0300

    Projeto Criado

---
+++ src/main/java/com/leandro/jogos/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.leandro.jogos;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JogosApplication.class);
+	}
+
+}
