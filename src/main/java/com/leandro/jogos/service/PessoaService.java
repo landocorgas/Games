8c9ff7ad01061386e477432f5fa18c30350cc3f2
landocorgas <landocorgas@outlook.com>
2022-08-13 13:51:10 -0300

    MYSQL Configurado

--- src/main/java/com/leandro/jogos/service/PessoaService.java
+++ src/main/java/com/leandro/jogos/service/PessoaService.java
@@ -3,6 +3,7 @@ package com.leandro.jogos.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.leandro.jogos.domain.Pessoa;
@@ -18,6 +19,26 @@ public class PessoaService {
 	public Pessoa find(Integer id) {
 		Optional<Pessoa> obj = repo.findById(id);
 		return obj.orElseThrow(()-> new ObjetoNaoEncontradoException("Objeto não encontrado.ID: "+ id +",Tipo: "+ Pessoa.class.getName()));
+		
+	}
+	
+	public Pessoa insert(Pessoa obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Pessoa update(Pessoa obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Pessoa possui produtos, não é possível deletar!");
+		}
 		
 	}
 }
